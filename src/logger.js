de0280410b2ccd1b2887506c72d6bcd9a7613ebf
Osnat Lazar <o.lazar@delekmotors.co.il>
2025-08-19 09:48:18 +0300

    Chnges wit bug

---
+++ src/logger.js
@@ -0,0 +1,16 @@
+
+// let logs = [];
+
+// export function log(message) {
+//   const timestamp = new Date().toISOString();
+//   logs.push({ timestamp, message });
+//   console.log(`${timestamp} - ${message}`); // זה עדיין יודפס ב־console
+// }
+
+// export function exportLogsJSON() {
+//   return JSON.stringify(logs, null, 2);
+// }
+
+// export function clearLogs() {
+//   logs = [];
+// }
